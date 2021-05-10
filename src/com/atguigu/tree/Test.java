cee2cd82bd93153337b89abb1e7257075a16ad4b
qiaobuyun <qbyun130@163.com>
2021-05-10 17:13:39 +0800

    import

--- src/com/atguigu/tree/Test.java
+++ src/com/atguigu/tree/Test.java
@@ -1,11 +1,23 @@
 package com.atguigu.tree;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Test {
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
+		List<String> list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		list.add("6");
+		list.add("7");
+		list.add("8");
+		list.add("9");
+		list.add("10");
+		list.add("11");
 	}
 }
